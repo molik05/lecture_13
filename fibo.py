30df446c76cc28e40672a48582642fa053247514
molik05 <xmolik05@vutbr.cz>
2021-05-03 10:46:14 +0200

    Fibonacci

--- fibo.py
+++ fibo.py
@@ -1,7 +1,17 @@
+def recursivve_nth_fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return recursivve_nth_fibo(n - 1) + recursivve_nth_fibo(n-2)
 
 def main():
-    pass
-
+    n = int(input("Zadejte poradi hledaneho prvku Fibonacciho posloupnosti: "))
+    fib_seq = []
+    for x in range(n+1):
+        fib_seq.append(recursivve_nth_fibo(x))
+    print(fib_seq)
 
 if __name__ == '__main__':
     main()
